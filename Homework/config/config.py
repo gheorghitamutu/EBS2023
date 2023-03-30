8277dc991dd6f9b57d31a4f391b4dfb6d284f5e6
Gheorghiță Mutu <gheorghitamutu@gmail.com>
2023-03-30 15:18:56 +0300

    [Homework] + added initial project structure, config, requirements (using numpy & mqtt)

---
+++ Homework/config/config.py
@@ -0,0 +1,23 @@
+from constants.constants import *
+
+SUBSCRIPTIONS_COUNT = 2000
+PUBLICATIONS_COUNT = 4000
+
+FREQUENCY_OF_EQUALS_OPERATION_PER_CITIES_SUBSCRIPTIONS = 0.7
+
+frequency_weights = {
+    FieldsType.STATION_ID: 0.7,
+    FieldsType.CITY: 0.9,
+    FieldsType.TEMP: 0.3,
+    FieldsType.RAIN: 0.4,
+    FieldsType.WIND: 0.5,
+    FieldsType.DIRECTION: 0.6,
+    FieldsType.DATE: 0.7
+}
+
+complex_subscriptions_frequency = {
+    ComplexPublication.CITY: 1,
+    ComplexPublication.AVERAGE_TEMPERATURE: 0.9,
+    ComplexPublication.AVERAGE_RAIN: 0.1,
+    ComplexPublication.AVERAGE_WIND: 0.2
+}
---
+++ Homework/constants/constants.py
@@ -0,0 +1,48 @@
+import enum
+from datetime import datetime, timedelta
+
+SUBSCRIPTIONS_FILEPATH = "./output/subscriptions_file.txt"
+PUBLICATIONS_FILEPATH = "./output/publications_file.txt"
+
+STATION_IDS = (i for i in range(1, 9))
+CITIES = ("Arad", "Timisoara", "Bucuresti", "Sibiu", "Brasov")
+WIND_DIRECTIONS = ("N", "S", "W", "E", "NW", "NE", "SW", "SE")
+NUMERIC_OPERATORS = (">", "<=", "<", "<=", "=")
+STRING_OPERATORS = ("=", "!=")
+
+MAXIMUM_TEMPERATURE = 45
+MINIMUM_TEMPERATURE = -25
+
+MAXIMUM_PROBABILITY = 1
+MINIMUM_PROBABILITY = 0
+
+MAXIMUM_WIND_SPEED_IN_KMS = 140
+MINIMUM_WIND_SPEED_IN_KMS = 0
+
+END_DATE = datetime.now()
+START_DATE = END_DATE - timedelta(days=730)
+
+MINIMUM_RAIN = 0.0
+MAXIMUM_RAIN = 100.0
+
+
+class FieldsType(enum.Enum):
+    STATION_ID = 1
+    CITY = 2
+    TEMP = 3
+    RAIN = 4
+    WIND = 5
+    DIRECTION = 6
+    DATE = 7
+
+
+class ComplexPublication(enum.Enum):
+    CITY = 8
+    AVERAGE_TEMPERATURE = 9
+    AVERAGE_RAIN = 10
+    AVERAGE_WIND = 11
+
+
+class FilterValueType(enum.Enum):
+    NUMERIC = 1
+    STRING = 2
