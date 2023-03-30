e51c0febe37cb23dbc512014458420b8dbdd2a34
Gheorghiță Mutu <gheorghitamutu@gmail.com>
2023-03-30 17:40:18 +0300

    [Homework] # fixed complex subscription string serialization

--- Homework/models/complex_subscription.py
+++ Homework/models/complex_subscription.py
@@ -19,7 +19,7 @@ class ComplexSubscription:
 
     def __str__(self) -> str:
         return f'{{{self.entities[ComplexPublication.CITY]}{self.entities[ComplexPublication.AVERAGE_TEMPERATURE]}' \
-               f'{self.entities[ComplexPublication.AVERAGE_RAIN]}{self.entities[ComplexPublication.AVERAGE_WIND]}'
+               f'{self.entities[ComplexPublication.AVERAGE_RAIN]}{self.entities[ComplexPublication.AVERAGE_WIND]}}}'
 
     def set_field(self, field_id):
         self.entities[field_id] = self.filters[field_id]
